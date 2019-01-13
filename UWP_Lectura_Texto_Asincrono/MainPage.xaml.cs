76d94f1e4fa31b59de35e65fba9fbea537b84cd4
Fran de la Jungla <fran.j2690@hotmail.com>
2019-01-13 12:07:11 +0100

    UWP Lectura Fichero Asincrono mostrando el Poercentaje de lectura y usando el Dispatcher para mostrar el valor del % por pantalla en tiempo real

--- UWP_Lectura_Texto_Asincrono/MainPage.xaml.cs
+++ UWP_Lectura_Texto_Asincrono/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,32 @@ namespace UWP_Lectura_Texto_Asincrono
         public MainPage()
         {
             this.InitializeComponent();
+            Debug.WriteLine(ApplicationData.Current.LocalFolder);
+        }
+
+        private async void Lectura_Fichero (object sender, RoutedEventArgs e)
+        {
+            String textoFinal = "";
+            var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var file = await folder.GetFileAsync("Educación_Fundamental.txt");
+            var readFile = await Windows.Storage.FileIO.ReadLinesAsync(file);
+            
+            Debug.WriteLine("Total lineas: " + readFile.Count);
+            for (int i = 1; i <= readFile.Count; i++)
+            {
+                textoFinal += "\n" + readFile[i - 1];
+
+                float percent = ((float)i / readFile.Count ) * 100;
+                Debug.WriteLine (percent);
+                Debug.WriteLine(readFile[i - 1]);
+                Windows.UI.Core.DispatchedHandler porcentajeTexto = () =>
+                {
+                    texto_porcentaje.Text = percent.ToString();
+                };
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, porcentajeTexto);
+            }
+
+            texto_Fichero.Text = textoFinal;
         }
     }
 }
